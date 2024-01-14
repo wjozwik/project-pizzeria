b445bfbbac680e1a87da8e544b3c3a7acbc1437e
Wojciech Jozwik <wj.telefon@gmail.com>
2024-01-14 12:13:12 +0100

    Add booking subpage

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,63 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
-  initData: function() {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+
+        const clickedElement = this;
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initData: function(){
     const thisApp = this;
     
     thisApp.data = {};
@@ -26,7 +80,7 @@ const app = {
       // console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
   
-  initMenu: function() {
+  initMenu: function(){
     const thisApp = this;
 
     // console.log('thisApp.data:', thisApp.data);
@@ -36,7 +90,7 @@ const app = {
     }
   },
 
-  initCart: function() {
+  initCart: function(){
     const thisApp = this;
 
     const cartElem = document.querySelector(select.containerOf.cart);
@@ -49,13 +103,16 @@ const app = {
     })
   },
 
-  init: function() {
+  init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
     // console.log('thisApp:', thisApp);
     // console.log('classNames:', classNames);
     // console.log('settings:', settings);
     // console.log('templates:', templates);
+
+    thisApp.initPages();
+
     thisApp.initData();
     thisApp.initCart();
   },
